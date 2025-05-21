d7cd905ecfe21fcb87995af7c09b86ca59f39479
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-05-21 09:41:30 +0530

    cors enabling

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://mern-task-manager-app.netlify.app", "http://localhost:3000", "http://localhost:3001","http://localhost:5173"],
+    origin: ["https://mern-task-manager-app.netlify.app","https://taskmanager-backend-unca.onrender.com", "http://localhost:3000", "http://localhost:3001","http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
